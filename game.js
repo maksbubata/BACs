6aee6a1b167a559d6fe3c779471957a2d240b77c
John Doe <johndoe@example.com>
2024-02-12 19:45:18 +0200

    number regex added

--- game.js
+++ game.js
@@ -1,16 +1,36 @@
-document.addEventListener('click', event=>{
-    const subusern = event.target;
-    if (subusern.className==="submt"){
-      subusern.parentElement.parentElement.remove();
-      
-    }
-  })
-  let username;
-  window.addEventListener('DOMContentLoaded', function() {
-    username = document.getElementById('subusern');
-    username.addEventListener('click', func);
 
-    function func() {
-      document.getElementById("plname").innerHTML = document.getElementById("gamertag").value
-    }
-  });
+  let username;
+  let playernum;
+  const numbr = new RegExp(/^(?=[1-9])(?:([0-9])(?!.*\1)){4}$/gm)
+
+  window.addEventListener('DOMContentLoaded', function() {
+    
+        username = document.getElementById('subusern');
+        username.addEventListener('click', func);
+
+        function func() {
+        document.getElementById("plname").innerHTML = document.getElementById("gamertag").value
+        }
+    
+    document.addEventListener('click', event=>{
+        const subusern = event.target;
+        if (subusern.className==="submt"){
+        subusern.parentElement.parentElement.remove();
+        
+        }
+    })
+    document.addEventListener ('click',event=>{
+        const subnum = event.target;
+        if (subnum.className==="submnum"){
+            playernum = document.getElementById('playernum').value
+            if (numbr.test(playernum)){
+                document.getElementById('numb1').innerHTML = playernum
+            }
+            
+            else{
+                document.getElementById('wrngnum').innerHTML = 'Number not matching rules. Please check the rules'
+            }
+        }
+    })
+});
+  
